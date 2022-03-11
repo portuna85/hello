e8c46da1e52d272b9e53af3a47688037ef74ff67
portuna85 <portuna85@gmail.com>
2022-03-11 16:06:00 +0900

    Spring MVC UPDATE

--- src/main/java/hello/springmvc/basic/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/ResponseViewController.java
@@ -2,9 +2,7 @@ package hello.springmvc.basic;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
