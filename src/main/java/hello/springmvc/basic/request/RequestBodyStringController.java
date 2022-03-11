de44541443d65edd63206f186d305ba7a3608dca
portuna85 <portuna85@gmail.com>
2022-03-11 19:14:27 +0900

    Spring MVC UPDATE

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -7,6 +7,7 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.HttpEntityMethodProcessor;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
